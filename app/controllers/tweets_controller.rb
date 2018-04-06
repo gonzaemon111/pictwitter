9a92c1f3aeb1800113b7861c120ce46435aab884
GokiSakuraba <goki14914@icloud.com>
2018-04-06 22:16:22 +0900

    Edit app/controllers/tweets_controller.rb
    Add before_action :move_to_index
    Add private def move_to_index

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,17 +1,24 @@
-class TweetsController < ApplicationController
-  def index
-    @tweets = Tweet.all.order("id DESC").page(params[:page]).per(5)
-  end
+  class TweetsController < ApplicationController
 
-  def new
-  end
+    before_action :move_to_index, except: :index
 
-  def create
-    Tweet.create(name: tweet_params[:name], image: tweet_params[:image], text: tweet_params[:text])
-  end
+    def index
+      @tweets = Tweet.page(params[:page]).per(5).order("created_at DESC")
+    end
 
-  private
-  def tweet_params
-    params.permit(:name, :image, :text)
+    def new
+    end
+
+    def create
+      Tweet.create(tweet_params)
+    end
+
+    private
+    def tweet_params
+      params.permit(:name, :image, :text)
+    end
+
+    def move_to_index
+      redirect_to action: :index unless user_signed_in?
+    end
   end
-end
