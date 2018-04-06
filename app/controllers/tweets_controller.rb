6743bb620ac88bf03fe8eb02c55e154f0c8951aa
GokiSakuraba <goki14914@icloud.com>
2018-04-06 21:49:17 +0900

    Edit app/controllers/tweets_controller.rb
    Add  def new
    Add  def create
    Add  private twee_params

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,16 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all.order("id DESC")
   end
+
+  def new
+  end
+
+  def create
+    Tweet.create(name: tweet_params[:name], image: tweet_params[:image], text: tweet_params[:text])
+  end
+
+  private
+  def tweet_params
+    params.permit(:name, :image, :text)
+  end
 end
