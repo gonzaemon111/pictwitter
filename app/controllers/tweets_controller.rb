8f80e6f7dc75969f4320df309a068db58a192340
GokiSakuraba <goki14914@icloud.com>
2018-04-15 01:31:37 +0900

    Edit app/controllers/tweets_contoller.rb
    Add definition of destroy

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -13,6 +13,11 @@
       Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
     end
 
+    def destroy
+      tweet = Tweet.find(params[:id])
+      tweet.destroy   if current_user.id == tweet.user_id
+    end
+
     private
     def tweet_params
       params.permit(:image, :text)
