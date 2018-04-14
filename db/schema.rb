fc8f459c1fa603baeb9c2e7d256a8a165706dc2b
GokiSakuraba <goki14914@icloud.com>
2018-04-15 01:15:14 +0900

    Edit db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180406133421) do
+ActiveRecord::Schema.define(version: 20180414161412) do
 
   create_table "tweets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "name"
     t.text "text"
     t.text "image"
     t.datetime "created_at", null: false
